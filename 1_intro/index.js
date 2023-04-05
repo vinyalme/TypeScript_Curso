a0e3650230d1186fd35ba40c06a51a794bac543f
Vinicius Almeida <vialmeida04@gmail.com>
2023-04-05 12:53:14 -0300

    primeiro pagina de ts

---
+++ 1_intro/index.js
@@ -0,0 +1,9 @@
+var firstName = "Fernando";
+var lastName = 4;
+var x = true;
+function greeting(nome) {
+    console.log("Olá " + nome);
+}
+greeting(firstName);
+//greeting(lastName)
+//greeting(x)
---
+++ 1_intro/index.ts
@@ -0,0 +1,11 @@
+const firstName ="Fernando"
+const lastName = 4
+const x = true
+
+function greeting(nome: string) {
+    console.log("Olá " + nome)
+}
+
+greeting(firstName)
+//greeting(lastName)
+//greeting(x)
